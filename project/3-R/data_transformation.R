9c0ed7fe15f36c0dff1d6a8fd4fedf302787c512
Domas Taparauskas <domas.taparauskas@gmail.com>
2023-12-28 18:55:09 +0200

    Update data_transformation.R

--- project/3-R/data_transformation.R
+++ project/3-R/data_transformation.R
@@ -1,3 +1,14 @@
-library(readr)
-read_csv("../1-data/put_your_data_files_here.csv")
-### save combined file into 1-data directory
+#file import
+sample = read.csv("1-sample_data.csv")
+additional_data = read.csv("2-additional_data.csv")
+additional_features = read.csv("3-additional_features.csv")
+
+#Merge files
+set.seed(99)
+dfsample <- data.frame(sample)
+dfaddit <- data.frame(additional_data)
+dffeatures <- data.frame(additional_features)
+dfdata <- rbind(dfsample, dfaddit)
+
+df <- merge(dfdata, dffeatures, by = "id")
+df<-subset(df, select = -c(id))
